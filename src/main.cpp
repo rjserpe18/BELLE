8e1c8e36fa8188a22bbe812a9271e3bef8749f6a
rjserpe18 <rexserpe@gmail.com>
2020-11-14 16:29:18 -0500

    Pulling indicator pin low for testing before moving to galvoDev

--- src/main.cpp
+++ src/main.cpp
@@ -13,31 +13,6 @@
 
 using namespace std;
 
-//using the first four rows of the second block of the CN8 header (on the dev board)
-DigitalOut clockPlus(PA_3);
-DigitalOut clockMinus(PD_7);
-
-DigitalOut syncPlus(PC_0);
-DigitalOut syncMinus(PD_6);
-
-// DigitalOut xPlus(PC_3);
-// DigitalOut xMinus(PD_5);
-
-// DigitalOut yPlus(PC_1);
-// DigitalOut yMinus(PD_4);
-
-
-
-
-
-
-DigitalOut xPlus(PF_3);
-DigitalOut xMinus(PD_4);
-
-DigitalOut yPlus(PC_3);
-DigitalOut yMinus(PD_5);
-
-
 //define LED activation representation pins
 DigitalOut tLLED(PF_8);
 DigitalOut tRLED(PF_7);
@@ -49,6 +24,9 @@ AnalogIn tL(A2);
 AnalogIn bL(A3);  
 AnalogIn tR(A4);  
 AnalogIn bR(A5);  
+
+//Communication pin to the galvo MCU
+DigitalOut activation_indicator(PG_0);
 
 //initialize serial monitor
 Serial mac2(USBTX, USBRX);
@@ -70,140 +48,6 @@ float wait_time = period/stepCount;
 
 //One half of the clocking period, effectively. Steptime of 1us gives 2us period = 0.5Mhz 
 int stepTime = 2;  //in microseconds (using wait_us())
-
-void coordsTo16(){    
-    //this could likely be sped up...
-    xFloat = xFloat/2;
-    yFloat = yFloat/2;
-    
-    x = (0.5 + xFloat) * UINT16_MAX;
-    y = (0.5 + yFloat) * UINT16_MAX;
-}
-
-void write(){
-
-    coordsTo16();
-
-    //flip the sync signal 
-    syncPlus = !syncPlus;
-    syncMinus = !syncMinus; 
-
-            //Control Bit 1
-
-    //pull clock high so that the data bits can change
-    clockPlus = !clockPlus;
-    clockMinus = !clockMinus;
-
-    xPlus = 0;
-    xMinus = !xPlus;
-
-    yPlus = 0;
-    yMinus = !yPlus;
-
-    wait_us(stepTime);
-
-    //pull clock low to sample the bits 
-    clockPlus = !clockPlus;
-    clockMinus = !clockMinus;
-
-    wait_us(stepTime);
-
-            //Control Bit 2
-
-    //pull clock high so that the data bits can change
-    clockPlus = !clockPlus;
-    clockMinus = !clockMinus;
-
-    //don't need to do any changing, x and y control bits are the same
-
-    wait_us(stepTime);
-
-    //pull clock low to sample the bits 
-    clockPlus = !clockPlus;
-    clockMinus = !clockMinus;
-
-    wait_us(stepTime);
-
-                //Control Bit 3
-
-    //pull clock high so that the data bits can change
-    clockPlus = !clockPlus;
-    clockMinus = !clockMinus;
-
-    //flipping control bits to 1 
-    xPlus = !xPlus;
-    xMinus = !xPlus;
-
-    yPlus = !yPlus;
-    yMinus = !yPlus;
-
-    wait_us(stepTime);
-
-    //pull clock low to sample the bits 
-    clockPlus = !clockPlus;
-    clockMinus = !clockMinus;
-
-    wait_us(stepTime);
-
-    //now sending the control bits
-    for(int i=0; i<16; i++){
-
-        //pull clock high to change bits
-        clockPlus = !clockPlus;
-        clockMinus = !clockMinus;
-
-        xPlus = (x>>(15-i))&1;
-        xMinus = !xPlus;
-        
-        yPlus = (y>>(15-i))&1;
-        yMinus = !yPlus;
-
-        wait_us(stepTime);
-
-        //pull clock low to sample the bits 
-        clockPlus = !clockPlus;
-        clockMinus = !clockMinus;
-
-        wait_us(stepTime);
-    }
-
-    //pull clock high to change bits
-    clockPlus = !clockPlus;
-    clockMinus = !clockMinus;
-
-    //send the parity bit --- after some testing, seems to not really do anything?? We'll just set it to zero.
-    // xPlus = xParity;
-    // xMinus = !xPlus;
-
-    xPlus = 0;
-    xMinus = 1;
-    
-    yPlus = 0;
-    yMinus = 1;
-
-
-    // yPlus = yParity;
-    // yMinus = !yPlus;
-
-    //flip the sync bit to signal communication of parity bit
-    syncPlus = !syncPlus;
-    syncMinus = !syncPlus;
-
-    wait_us(stepTime);
-
-    //pull the clock low to read parity bits
-    clockPlus = !clockPlus;
-    clockMinus = !clockMinus;
-
-    wait_us(stepTime);
-
-    // //before we end, we'll increment x and y to see
-    // x = x + 0b101;
-    // y = y + 0b101;
-
-    //now we're ready to start over. The next cycle starts by pulling sync and clock high, 
-    //so we don't need to do that here
-}
 
 void updateFrequency( float newFrequency){
     frequency = newFrequency;
@@ -257,7 +101,10 @@ int main(){
         array.sensors[i].threshold = 1;
     }
 
+    activation_indicator = 0;
+
     array.calibrate();
+    array.blink();
     mac2.printf("\n\n");
     mac2.printf("Initial calibration \r\n");
     array.printCalibration();
@@ -275,19 +122,5 @@ int main(){
             i=0;
         }
         wait_sec(0.01);
-
-        // ab +=1;
-
-        // if(ab == 20){
-        //     array.displayValue();
-        //     ab == 0;
-        // }
-
-        // mac2.printf("tl: %f  ",tL.read());
-        // mac2.printf("tR: %f  ",tR.read());
-        // mac2.printf("bR: %f  ",bR.read());
-        // mac2.printf("bL: %f  \r\n",bL.read());
-
-        
     }
 }
